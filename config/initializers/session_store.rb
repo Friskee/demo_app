10a9f36019ad7442dc6b0b04b16326cf87320da8
Matthew Sorrell <matthewsorrell88@gmail.com>
2014-05-26 08:06:15 -0700

    Finished app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-<<<<<<< HEAD
+
 DemoApp::Application.config.session_store :cookie_store, key: '_demo_app_session'
-=======
-Rails.application.config.session_store :cookie_store, key: '_demo_app_session'
->>>>>>> f897052cc9019ca23a9ad8a32e917f319ba3fa29
+
